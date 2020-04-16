95acfc4e706fc120d186a5d85f5a07187825178a
Cleber Gomes <fclebinho@gmail.com>
2020-04-15 22:02:39 -0300

    Desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
 
+import api from './services/api';
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    api.post('repositories', {
+      title: "Mobile com ReactJS",
+      techs: ["React", "Native"]
+    }).then(({ data }) => setRepositories([...repositories, data]));
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then(() => {
+      const repos = repositories.filter(repository => repository.id !== id);
+      setRepositories(repos);
+    })
   }
+
+  useEffect(() => {
+    api.get('repositories').then(({ data }) => setRepositories(data));
+  }, [])
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
